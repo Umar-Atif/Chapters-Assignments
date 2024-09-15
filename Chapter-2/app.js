e5cb8d16e21143d6a2ab1a1644f2e2f172753b88
Umar-Atif <umaratifweb@gmail.com>
2024-09-15 23:19:56 +0500

    Chapter 2 Completed!

---
+++ Chapter-2/app.js
@@ -0,0 +1,43 @@
+// Qno.1 Answer
+var username = "Your-Name";
+
+// Qno.2 Answer
+var myName = "Umar";
+
+// Qno.3 Answer
+var message = "Hello World";
+alert(message);
+
+// Qno.4 Answer
+var name = "Umar";
+var age = "15 years old";
+var skill = "HTML , CSS , JS";
+alert(name);
+alert(age);
+alert(skill);
+
+// Qno.5 Answer
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// Qno.6 Answer
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// Qno.7 Answer
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Qno.8 Answer
+var head = document.querySelector("h3");
+head.innerHTML = "Yah! I can write HTML content through JavaScript";
+
+// Qno.9 Answer
+var decorate = `---------@%&%@---------`;
+alert(decorate);
+
+
+
+
+
+
