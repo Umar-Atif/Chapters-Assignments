f1414de18e2a645cd13a0bc41a1b8c7508b44791
Umar-Atif <umaratifweb@gmail.com>
2024-09-15 21:17:08 +0500

    Chapter-1 Completed!

---
+++ Chapter-1/app.js
@@ -0,0 +1,26 @@
+// Qno.1 Answer:
+alert("Welcome");
+
+// Qno.2 Answer:
+alert("Error! Please enter a valid password."); 
+
+// Qno.3 Answer:
+var alert1 = `Welcome to JS Land...
+Happy Coding!`;
+alert(alert1);
+
+// Qno.4 Answer:
+alert("Welcome to JS Land...");
+var p = `Happy Coding!
+
+Prevent this page from creating additional dialogs`;
+alert(p);
+
+// Qno.5 Answer:
+alert("Hello... I can run JS through my web browser's console");
+
+// Qno.6 Answer:
+Ok
+
+// Qno.7 Answer:
+Ok
